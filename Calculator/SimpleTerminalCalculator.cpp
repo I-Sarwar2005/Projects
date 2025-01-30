91fe65e5be5dd96e1524807c6795e19549986845
I-Sarwar2005 <ibraheem.s.sarwar@gmail.com>
2025-01-29 22:33:14 -0800

    First Commit

---
+++ Calculator/SimpleTerminalCalculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    string operation;
+    float number1, number2;
+    // double currentAnswer;
+
+    system("clear");
+    cout << "Calculator app" << endl;
+    cout << "Please enter your numbers and Operation(+, -, x, / or ÷): ";
+    cin >> number1;
+    while(cin >> operation){
+    if (operation != "+" && operation != "-" && operation != "x" && operation != "/" && operation != "÷"){
+        cout <<  "Invalid input try again" << endl;
+    }else if (operation == "+" || operation == "-" || operation == "x" || operation == "/" || operation == "÷"){
+            break;
+        }
+    }
+    cin >> number2;
+    if (operation == "+"){
+        cout << "FInal Answer: " <<  number1 + number2 << endl;
+    }else if (operation == "-"){
+        cout << "FInal Answer: " <<  number1 - number2 << endl;
+    }else if (operation == "x"){
+        cout << "FInal Answer: " <<  number1 * number2 << endl;
+    }else if (operation == "/" || operation == "÷"){
+        cout << "FInal Answer: " <<  number1 / number2 << endl;
+    }
+    
+
+
+    return 0;
+}
